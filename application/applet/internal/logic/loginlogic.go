cf45ed9185e438fba98af98ac76358f0669d7ca8
Wg <1448896477@qq.com>
2024-04-23 20:47:44 +0800

    user

--- application/applet/internal/logic/loginlogic.go
+++ application/applet/internal/logic/loginlogic.go
@@ -1,7 +1,13 @@
 package logic
 
 import (
+	"WgZhihu/application/applet/internal/code"
+	"WgZhihu/application/user/rpc/user"
+	"WgZhihu/pkg/encrypt"
+	"WgZhihu/pkg/jwt"
+	"WgZhihu/pkg/xcode"
 	"context"
+	"strings"
 
 	"WgZhihu/application/applet/internal/svc"
 	"WgZhihu/application/applet/internal/types"
@@ -24,7 +30,54 @@ func NewLoginLogic(ctx context.Context, svcCtx *svc.ServiceContext) *LoginLogic 
 }
 
 func (l *LoginLogic) Login(req *types.LoginRequest) (resp *types.LoginResponse, err error) {
-	// todo: add your logic here and delete this line
+	req.Mobile = strings.TrimSpace(req.Mobile)
+	if len(req.Mobile) == 0 {
+		return nil, code.LoginMobileEmpty
+	}
+	req.VerificationCode = strings.TrimSpace(req.VerificationCode)
+	if len(req.VerificationCode) == 0 {
+		return nil, code.VerificationCodeEmpty
+	}
+	//检查验证码
+	err = checkVerificationCode(l.svcCtx.BizRedis, req.Mobile, req.VerificationCode)
+	if err != nil {
+		return nil, err
+	}
+	//手机号检查
+	mobile, err := encrypt.EncMobile(req.Mobile)
+	if err != nil {
+		logx.Errorf("EncMobile mobile: %s error: %v", req.Mobile, err)
+		return nil, err
+	}
+	//通过rpc从数据库中读取用户信息
+	u, err := l.svcCtx.UserRPC.FindByMobile(l.ctx, &user.FindByMobileRequest{Mobile: mobile})
+	if err != nil {
+		logx.Errorf("FindByMobile error: %v", err)
+		return nil, err
+	}
+	if u == nil || u.UserId == 0 {
+		return nil, xcode.AccessDenied
+	}
+	//获取jwt
+	token, err := jwt.BuildTokens(jwt.TokenOptions{
+		AccessSecret: l.svcCtx.Config.Auth.AccessSecret,
+		AccessExpire: l.svcCtx.Config.Auth.AccessExpire,
+		Fields: map[string]interface{}{
+			"userId": u.UserId,
+		},
+	})
+	if err != nil {
+		return nil, err
+	}
 
-	return
+	//删掉验证码的缓存
+	_ = delActivationCache(req.Mobile, req.VerificationCode, l.svcCtx.BizRedis)
+
+	return &types.LoginResponse{
+		UserId: u.UserId,
+		Token: types.Token{
+			AccessToken:  token.AccessToken,
+			AccessExpire: token.AccessExpire,
+		},
+	}, nil
 }
--- application/applet/internal/logic/userinfologic.go
+++ application/applet/internal/logic/userinfologic.go
@@ -1,7 +1,9 @@
 package logic
 
 import (
+	"WgZhihu/application/user/rpc/user"
 	"context"
+	"encoding/json"
 
 	"WgZhihu/application/applet/internal/svc"
 	"WgZhihu/application/applet/internal/types"
@@ -24,7 +26,23 @@ func NewUserInfoLogic(ctx context.Context, svcCtx *svc.ServiceContext) *UserInfo
 }
 
 func (l *UserInfoLogic) UserInfo() (resp *types.UserInfoResponse, err error) {
-	// todo: add your logic here and delete this line
-
-	return
+	userId, err := l.ctx.Value(types.UserIdKey).(json.Number).Int64()
+	if err != nil {
+		return nil, err
+	}
+	if userId == 0 {
+		return &types.UserInfoResponse{}, nil
+	}
+	u, err := l.svcCtx.UserRPC.FindById(l.ctx, &user.FindByIdRequest{
+		UserId: userId,
+	})
+	if err != nil {
+		logx.Errorf("FindById userId: %d error: %v", userId, err)
+		return nil, err
+	}
+	return &types.UserInfoResponse{
+		UserId:   u.UserId,
+		Username: u.Username,
+		Avatar:   u.Avatar,
+	}, nil
 }
